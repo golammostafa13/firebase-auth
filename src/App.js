16d92136014bebf46463185f2d4597a8f3d3052c
Mostafa <mostafa@ubuntu>
2021-03-01 07:38:09 +0600

    signIn and signOut

--- src/App.js
+++ src/App.js
@@ -1,9 +1,57 @@
 import './App.css';
+import firebaseConfig from './components/FirebaseConfig/FirebaseConfig';
+import "firebase/auth";
+import firebase from "firebase/app";
+import { useState } from 'react';
 
+firebase.initializeApp(firebaseConfig);
+const  provider = new firebase.auth.GoogleAuthProvider();
 function App() {
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  });
+  const userSignedIn = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const {displayName, email, photoURL} = res.user;
+       const user = {
+         isSignedIn: true,
+         name: displayName,
+         email: email,
+         photo: photoURL,
+       };
+       setUser(user);
+      })
+    .catch(err => {
+      console.log(err.message);
+    })
+  };
+  const userSignedOut = () =>{
+    firebase.auth().signInWithPopup(provider)
+    .then(res => {
+      const user = {
+        isSignedIn: false,
+        name: '',
+        email: '',
+        photo: ''
+      }
+      setUser(user);
+    })
+  };
   return (
     <div className="App">
-      
+    {user.isSignedIn ? 
+      <button onClick = {userSignedOut}>Sign Out</button> 
+      : <button onClick = {userSignedIn}>Sign In</button>
+    }
+    <div>
+      <p>{user.name}</p>
+      <p>{user.email}</p>
+      <img src={user.photo} alt=""/>
+    </div>
     </div>
   );
 }
---
+++ src/components/FirebaseConfig/FirebaseConfig.js
@@ -0,0 +1,10 @@
+// For Firebase JavaScript SDK v7.20.0 and later, `measurementId` is an optional field
+const firebaseConfig = {
+    apiKey: "AIzaSyAfkwWS_LaY3YwkmvzyYx4cMjyRaAWxHjI",
+    authDomain: "ema-john-simple-46bc1.firebaseapp.com",
+    projectId: "ema-john-simple-46bc1",
+    storageBucket: "ema-john-simple-46bc1.appspot.com",
+    messagingSenderId: "491714275376",
+    appId: "1:491714275376:web:9a0c8188895c9c3f21a5f5"
+};
+export default firebaseConfig;
